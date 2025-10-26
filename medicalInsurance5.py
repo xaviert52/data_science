bf59330542c0cb6d04743fd55ec2652e444568d1
xaviert52 <duke.chavin@gmail.com>
2025-10-26 13:16:03 -0500

    med5 update

---
+++ medicalInsurance5.py
@@ -0,0 +1,32 @@
+names = ["Judith", "Abel", "Tyson", "Martha", "Beverley", "David", "Anabel"]
+estimated_insurance_costs = [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0]
+actual_insurance_costs = [1100.0, 2200.0, 3300.0, 4400.0, 5500.0, 6600.0, 7700.0]
+
+# Add your code here
+total_cost = 0
+
+#for i in actual_insurance_costs:
+#  total_cost += i
+x = 0
+while x < len(actual_insurance_costs):
+  total_cost += actual_insurance_costs[x]
+  x += 1
+
+average_cost = total_cost/len(actual_insurance_costs)
+
+print('Average insurance cost: ' + str(average_cost) +' dollars.')
+
+for i in range(len(names)):
+  name = names[i]
+  insurance_cost = actual_insurance_costs[i]
+  print('The insurance cost for ' + name + ' is ' + str(insurance_cost) + ' dollars.')
+  if insurance_cost > average_cost:
+    print('The insurance cost for ' + name + ' is above average by ' + str(abs(insurance_cost-average_cost)))
+  elif insurance_cost < average_cost:
+    print('The insurance cost for ' + name + ' is below average by ' + str(abs(insurance_cost-average_cost)))
+  else:
+    print('The insurance cost for ' + name + ' is equal to the average')
+
+updated_estimated_costs = [x*(11/10) for x in estimated_insurance_costs]
+
+print(updated_estimated_costs)
